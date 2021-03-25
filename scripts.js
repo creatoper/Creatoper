7046132edc3b8c7e42933b88e2690aeebb2e17b7
Bui Quang Bao <60503568+buiquangbao@users.noreply.github.com>
2021-03-25 09:46:45 +0700

    Update

--- scripts.js
+++ scripts.js
@@ -1,2 +1,7 @@
 // Test
 document.getElementById("test-scripts").innerHTML = "> scripts.js load successfully!";
+
+// Service-worker register
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../sw.js');
+}
---
+++ sw.js
@@ -0,0 +1,7 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'image',
+    new workbox.strategies.NetworkFirst()
+    // CacheFirst or NetworkFirst strategy
+)
